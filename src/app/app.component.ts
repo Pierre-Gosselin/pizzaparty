b3c1c812289c92094a50eb97b54f81a392a2936d
pierre gosselin <pierre.gosselin@orange.fr>
2019-07-30 12:30:43 +0200

    Ajout interface pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,16 @@ export class Pizza {
   id: number;
   name: string;
   price: number;
+  image: string;
+  description: string;
 }
+
+const PIZZAS : Pizza[] = [
+  { id: 1, name: 'Reine', price: 12, image: "assets/queen.jpg", description: "Sauce tomate, mozzarella, jambon et double champignons frais."},
+  { id: 2, name: '4 fromages', price: 13, image: "assets/fromage.jpg", description: "Sauce tomate ou crème fraîche légère, mozzarella, fromage de chèvre, emmental et Fourme d'Ambert AOP." },
+  { id: 3, name: 'Orientale', price: 11, image: "assets/orientale.jpg", description: "Sauce tomate, mozzarella, double merguez** et champignons frais." },
+  { id: 4, name: 'Cannibale', price: 9, image: "assets/cannibale.jpg", description: "Sauce barbecue, mozzarella, boulettes au bœuf*, filet de poulet rôti et mariné et merguez**." }
+];
 
 @Component({
   selector: 'app-root',
@@ -13,9 +22,12 @@ export class Pizza {
 })
 export class AppComponent {
   title = 'pizzaparty';
-  pizza : Pizza = {
-    id: 1,
+  pizzas = PIZZAS;
+  pizza: Pizza={
+    id:1,
     name: '4 fromages',
-    price: 10
+    price: 10,
+    image: "no-image.jpg",
+    description: "pizza"
   }
 }
